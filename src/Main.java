5127d17fdc9856264fc12f528af7a3ecfa3335c1
DELL <tshiva0112@gmail.com>
2024-04-25 20:54:37 +0530

    loops

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        //for, while
+      //  for(int i=1;i<=10;i++){
+       //     System.out.println("Hello ");
+         //   for(int i= 1;i<5; i++){
+           //     System.out.println(i+1);
+//     for (int i= 2;i<=10; i=i+2){
+  //       System.out.println("i");
+    // }
+        // odd num
+        //int i=1;
+        //while (i <= 10) {
+           // System.out.println(i);
+         //   i=i+2;
+       // }
+        int i=1;
+        do{
+            System.out.println("i");
+            i=i+2;
+        }while(i<=10);
+            }
+        }
+    //}
